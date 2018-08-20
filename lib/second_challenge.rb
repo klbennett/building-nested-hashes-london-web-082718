e05559eab7d357ed2f5ae4b15d1c592c56a1f446
Kay B <kaylouisebennett@gmail.com>
2018-08-20 11:14:00 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,10 +4,10 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends []
+      :hero_friends => []
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends []
+      :heroine_friends => []
 end
